b8a27f6b56c724a5ade6d42187a5d517fab0f9d9
Zach Cowen <cowen.30@osu.edu>
2022-10-22 23:04:23 -0400

    updating some unit tests

--- src/app/components/main/stats/stats.component.spec.ts
+++ src/app/components/main/stats/stats.component.spec.ts
@@ -1,23 +1,42 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
+
+import createSpyObj = jasmine.createSpyObj;
+import SpyObj = jasmine.SpyObj;
 
 import { StatsComponent } from './stats.component';
+import { ResultsService } from 'src/app/services/results/results.service';
 
 describe('StatsComponent', () => {
-  let component: StatsComponent;
-  let fixture: ComponentFixture<StatsComponent>;
+	let component: StatsComponent;
+	let fixture: ComponentFixture<StatsComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ StatsComponent ]
-    })
-    .compileComponents();
+	let mockResultsService: SpyObj<ResultsService>;
 
-    fixture = TestBed.createComponent(StatsComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+	beforeEach(async () => {
+		mockResultsService = createSpyObj('ResultsService', ['getStats']);
+		mockResultsService.getStats.and.returnValue(of());
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+		await TestBed.configureTestingModule({
+			declarations: [
+				StatsComponent
+			],
+			providers: [
+				{
+					provide: ResultsService,
+					useValue: mockResultsService
+				}
+			]
+		})
+		.compileComponents();
+
+		fixture = TestBed.createComponent(StatsComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
+
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
+
 });
