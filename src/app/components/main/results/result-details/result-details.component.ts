915106276bf2e1940f1ee7fdbcd84d1109892a0a
Zach Cowen <cowen.30@osu.edu>
2022-10-30 17:43:38 -0400

    make result detail call when modal loads

--- src/app/components/main/results/result-details/result-details.component.ts
+++ src/app/components/main/results/result-details/result-details.component.ts
@@ -1,17 +1,30 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, ReplaySubject } from 'rxjs';
 import { DurationHelper } from 'src/app/helpers/duration.helper';
+import { LapDetail } from 'src/app/models/lap-detail.model';
 import { Result } from 'src/app/models/result.model';
+import { ResultDetailsService } from 'src/app/services/result-details/result-details.service';
 
 @Component({
 	selector: 'app-result-details',
 	templateUrl: './result-details.component.html',
 	styleUrls: ['./result-details.component.scss']
 })
-export class ResultDetailsComponent {
+export class ResultDetailsComponent implements OnInit {
 
-	@Input() result!: Result;
+	private _currentResult!: Result;
+
+	private _result: ReplaySubject<Result> = new ReplaySubject<Result>(1);
+
+	@Input('result') set result(value: Result) {
+		this._result.next(value);
+		this._currentResult = value;
+	}
+
+	get result(): Result {
+		return this._currentResult;
+	}
 
 	private _resultDetailModalShown = new BehaviorSubject<boolean>(false);
 
@@ -25,7 +38,18 @@ export class ResultDetailsComponent {
 
 	constructor(
 		public activeModal: NgbActiveModal,
-		public durationHelper: DurationHelper
+		public durationHelper: DurationHelper,
+		private resultDetailsService: ResultDetailsService
 	) {}
+
+	ngOnInit(): void {
+		this._result.subscribe((result) => {
+			if (result != null) {
+				this.resultDetailsService.getResultDetails(result.id).subscribe((lapDetails: LapDetail[]) => {
+					this._currentResult.lapDetails = lapDetails;
+				});
+			}
+		});
+	}
 
 }
