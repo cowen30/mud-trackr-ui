0931a0de1379ae095625fa7e03b5cdf3c55f103b
Zach Cowen <cowen.30@osu.edu>
2022-11-21 13:09:35 -0500

    fixing unit tests

--- src/app/services/result-details/result-details.service.spec.ts
+++ src/app/services/result-details/result-details.service.spec.ts
@@ -1,16 +1,28 @@
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { ResultDetailsService } from './result-details.service';
 
 describe('ResultDetailsService', () => {
-  let service: ResultDetailsService;
+	let resultDetailsService: ResultDetailsService;
+	let httpMock: HttpTestingController;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(ResultDetailsService);
-  });
+	beforeEach(() => {
+		TestBed.configureTestingModule({
+			imports: [
+				HttpClientTestingModule
+			]
+		});
+		resultDetailsService = TestBed.inject(ResultDetailsService);
+		httpMock = TestBed.inject(HttpTestingController);
+	});
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+	afterEach(() => {
+		httpMock.verify();
+	});
+
+	it('should be created', () => {
+		expect(resultDetailsService).toBeTruthy();
+	});
+
 });
